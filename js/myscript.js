1b743a5d38d93d68b66593556d124a6af37b3a0b
Thalita de Freitas <thalita.freitas@ufrj.br>
2022-10-14 21:58:01 -0300

    Botão de adicionar círculos funcionando

--- js/myscript.js
+++ js/myscript.js
@@ -19,12 +19,71 @@ adicionarImg.style.width = "40px";
 adicionarImg.style.height= "40px";
 
 let removerImg = document.createElement('img');
-removerImg.src = 'img/remover.png';
+removerImg.src = 'img/remove.png';
 removerImg.title = "Remover";
 removerImg.style.width = "40px";
 removerImg.style.height= "40px";
 
-//Criando botão
+
+//Criando div do botão
+let botaoDiv = document.createElement('div');
+botaoDiv.style.display = "block";
+botaoDiv.style.justifySelf = "left";
+botaoDiv.style.marginLeft = "10px";
+
+
+//Botão para adicionar círculos
+let botaoAdd = document.createElement('button');
+botaoAdd.type = 'button';
+botaoAdd.appendChild(adicionarImg);
+botaoAdd.style.display = 'block';
+
+
+//Botão para recomeçar
+let botaoRestart = document.createElement('button');
+botaoRestart.type = 'button';
+botaoRestart.appendChild(restartImg);
+botaoRestart.style.display = 'block';
+
+
+//Botão para remover círculos
+let botaoRemover = document.createElement('button');
+botaoRemover.type = 'button';
+botaoRemover.appendChild(removerImg);
+botaoRemover.style.display = 'block';
+
+
+//Adicionando botões na div de botões
+botaoDiv.appendChild(botaoAdd);
+botaoDiv.appendChild(botaoRemover);
+botaoDiv.appendChild(botaoRestart);
+
+//Criando div de círculos
+let circulos = document.createElement('div');
+circulos.style.float = "left";
+circulos.style.padding = "0.2em";
+circulos.style.margin = "1.0em auto";
+
+
+let listaCores = ['#483D8B', '#006400', '#DAA520']
+
+//Ao clicar no botão Adicionar
+botaoAdd.onclick = function(){
+    let circulo = document.createElement('div');
+    
+    //style circulo
+    circulo.style.width = '80px';
+    circulo.style.height = '80px';
+    circulo.style.float = 'right';
+    circulo.style.margin = '20px';
+    circulo.style.borderRadius = '50%';
+
+    let cor = listaCores[Math.floor(Math.random() * listaCores.length)];
+    circulo.style.background = cor;
+    circulos.appendChild(circulo);
+}
+
+
 
 
 
@@ -33,7 +92,6 @@ removerImg.style.height= "40px";
 document.addEventListener('DOMContentLoaded', function(event) {
     document.body.style.display = "grid";
     document.body.appendChild(titulo);
-    document.body.appendChild(restartImg);
-    document.body.appendChild(adicionarImg);
-    document.body.appendChild(removerImg);
+    document.body.appendChild(botaoDiv);
+    document.body.appendChild(circulos);
 });
